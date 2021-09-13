7ddd7ba3a169c49423dc3ad318ff086e0e5aea01
KevinAlmada <kevinalmada81@gmail.com>
2021-09-13 15:30:16 -0300

    primer commit

---
+++ public/javascript/header.js
@@ -0,0 +1,9 @@
+let menuToggle = document.getElementById("menu-toggle");
+
+function openRamiel() {
+    menuToggle.style.bottom = "-180%"
+}
+
+function closeRamiel() {
+    menuToggle.style.bottom = "100%"
+}
